a50166f705b549c1a2192a8aef0f72ca41d02cdc
S-kus <saumyakus@gmail.com>
2022-03-05 23:58:58 +0530

    added scrollbar and basic table

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import NavbarWrapper from "@components/navbar/Navbar";
 import { Fragment } from "react";
+import Table from "@components/table/table";
 
 export default function Home() {
     return (
         <Fragment>
             <NavbarWrapper>
                 <h1>Hellooo</h1>
+                <Table/>
             </NavbarWrapper>
         </Fragment>
     );
